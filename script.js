0548f95ed4c6a7dff1a97effd57936ccfb23a697
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-26 11:22:30 -0400

    Fetching input data

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form")
+const submit = document.querySelector("submit-button")
+const info = document.querySelector("#location-name").innerHTML
+console.log(info)
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault()
+
+    const locationInput = event.target[0].value
+
+
+
+    // const locationInput = document.querySelector("#location-input")
+    // console.log(locationInput.value)
+
+    fetch(`https://wttr.in/${locationInput}?format=j1`)
+    .then((response) => response.json())
+    .then((data) =>  {
+        let near = data.nearest_area[0]
+        let region = near.region[0].value
+        let country = near.country[0].value
+        let currently = data.current_condition[0].FeelsLikeF
+        const main = document.querySelector("#current-weather")
+
+        main.innerHTML = `
+            <p id="area">${locationInput}</p>
+            <p id="region">${region}</p>
+            <p id="country">${country}</p>
+            <p id="currently">${currently}</p>
+        `
+        console.log(main.textContent)
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
+form.reset();
+
+});
