00cea6efb98f6745f762654ff6c8d3aedb800075
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-26 12:23:08 -0400

    Added 3-Day Forecast To Webpage

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const form = document.querySelector("form")
 const submit = document.querySelector("submit-button")
-const info = document.querySelector("#location-name").innerHTML
-console.log(info)
+const main = document.querySelector("main")
+// const locationInput = document.querySelector("#location-input")
+// const info = document.querySelector("#location-name").innerHTML
+// console.log(info)
 
 form.addEventListener("submit", (event)=> {
     event.preventDefault()
-
+//Defining The Value Of The Input
     const locationInput = event.target[0].value
-
+// Removing Previous Search Text & Adding Input Into That Section As a List
     if(locationInput){
         if(document.querySelector("#no-search")){
             document.querySelector("#no-search").remove()
@@ -18,10 +20,6 @@ form.addEventListener("submit", (event)=> {
         unOrdered.prepend(search) 
     }
 
-
-    // const locationInput = document.querySelector("#location-input")
-    // console.log(locationInput.value)
-
     fetch(`https://wttr.in/${locationInput}?format=j1`)
     .then((response) => response.json())
     .then((data) =>  {
@@ -29,19 +27,54 @@ form.addEventListener("submit", (event)=> {
         let region = near.region[0].value
         let country = near.country[0].value
         let currently = data.current_condition[0].FeelsLikeF
-        const main = document.querySelector("#current-weather")
+        const article = document.querySelector("#current-weather")
 
-        main.innerHTML = `
+        article.innerHTML = `
             <p id="area">${locationInput}</p>
             <p id="region">${region}</p>
             <p id="country">${country}</p>
-            <p id="currently">${currently}</p>
+            <p id="currently">${currently}°F</p>
         `
-        console.log(main.textContent)
+// Adding 3-Day Forecast into Aside>Article Section
+        const upcoming = document.querySelectorAll(".upcoming")
+        const forecast =  data.weather
+        const today = forecast[0]
+        const tomorrow = forecast[1]
+        const after = forecast[2]
+    
+        upcoming[0].innerHTML = `
+            <h4>Today:</h4>
+            <h4>Average Temperature:</h4>
+            <p>${today.avgtempF}°F</p>
+            <h4>Max Temperature:</h4>
+            <p>${today.maxtempF}°F</p>
+            <h4>Min Temperature:</h4>
+            <p>${today.mintempF}°F</p>
+        `
+        upcoming[1].innerHTML = `
+            <h4>Tomorrow:</h4>
+            <h4>Average Temperature:</h4>
+            <p>${tomorrow.avgtempF}°F</p>
+            <h4>Max Temperature:</h4>
+            <p>${tomorrow.maxtempF}°F</p>
+            <h4>Min Temperature:</h4>
+            <p>${tomorrow.mintempF}°F</p>
+        `
+        upcoming[2].innerHTML = `
+            <h4>Day After Tomorrow: </h4>
+            <h4>Average Temperature:</h4>
+            <p>${after.avgtempF}°F</p>
+            <h4>Max Temperature:</h4>
+            <p>${after.maxtempF}°F</p>
+            <h4>Min Temperature:</h4>
+            <p>${after.mintempF}°F</p>
+        `
+        upcoming.append(main)
+
     })
     .catch((error) =>{
         console.log(error);
     });
-form.reset();
+    form.reset();
 
 });
