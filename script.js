f31a2d04f3a87f21e6b69bbf531ab5f4781c3221
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-28 10:48:53 -0400

    Weather Icon Coding

--- script.js
+++ script.js
@@ -5,29 +5,58 @@ const main = document.querySelector("main")
 // const info = document.querySelector("#location-name").innerHTML
 // console.log(info)
 
-form.addEventListener("submit", (event)=> {
-    event.preventDefault()
+const searchWeather = (locationInput) => {
+    
 //Defining The Value Of The Input
-    const locationInput = event.target[0].value
 
     fetch(`https://wttr.in/${locationInput}?format=j1`)
     .then((response) => response.json())
-    .then((data) =>  {
-
-// Weather Information Fetched from Link Being Displayed on Webpage
+    .then((data) => {
         let near = data.nearest_area[0]
         let region = near.region[0].value
         let country = near.country[0].value
         let currently = data.current_condition[0].FeelsLikeF
+        const forecast = data.weather
+        const today = forecast[0]
+        const tomorrow = forecast[1]
+        const after = forecast[2]
         const article = document.querySelector("#current-weather")
+        const upcoming = document.querySelectorAll(".upcoming")
+
+    
+// Weather Information Fetched from Link Being Displayed on Main
+        //Adding Chance of Data
+        let hour = data.weather[0].hourly[0]
+        let sun = hour.chanceofsunshine
+        let rain = hour.chanceofrain
+        let snow = hour.chanceofsnow
+        let weatherIcon = document.querySelector("img")
+            
+        if(sun > 50){
+            weatherIcon.src="./assets/icons8-summer.gif"
+            weatherIcon.alt="sun"
+        } else if (rain > 50){
+            weatherIcon.src="./assets/icons8-torrential-rain.gif"
+            weatherIcon.alt="rain"
+        } else if (snow > 50) {
+            weatherIcon.src="./assets/icons8-light-snow.gif"
+            weatherIcon.alt="snow"
+        } else {
+            weatherIcon.src="./assets/icons8-wind.gif"
+            weatherIcon.alt="wind"
+        }
 
         article.innerHTML = `
-            <p id="area"><strong>Nearest Area:</strong> ${locationInput}</p>
+                ${weatherIcon.outerHTML}           
+            <h2 id="area">${locationInput}</h2>
+            <p id="nearest"><strong>Nearest Area:</strong> ${locationInput}</p>
             <p id="region"><strong>Region:</strong> ${region}</p>
             <p id="country"><strong>Country:</strong> ${country}</p>
-            <p id="currently"><strong>Currently:</strong> ${currently}°F</p>
+            <p id="currently"><strong>Currently:</strong> Feels Like ${currently}°F</p>
+            <p id="sunshine"><strong>Chance of Sunshine:</strong> ${sun}%</p>
+            <p id="rain"><strong>Chance of Rain:</strong> ${rain}%</p>
+            <p id="snow"><strong>Chance of Snow:</strong> ${snow}%</p>
         `
-
 // Removing Previous Search Text & Adding Input Into That Section As a List
         if(locationInput){
             if(document.querySelector("#no-search")){
@@ -36,7 +65,13 @@ form.addEventListener("submit", (event)=> {
            const unOrdered = document.querySelector(".weather-history ul")
            const search = document.createElement("li")
            const link = document.createElement("a")
-           link.setAttribute("href","http://127.0.0.1:5500/index.html?")
+         
+
+           link.addEventListener("click", (event) =>{
+            searchWeather(locationInput, false)
+           })
+
+           link.setAttribute("href", "#")
            link.innerText = `${locationInput}`
            search.innerText = ` - ${currently}°F`
            unOrdered.prepend(search) 
@@ -44,69 +79,77 @@ form.addEventListener("submit", (event)=> {
        }
 
 // Adding 3-Day Forecast into Aside>Article Section
-        const upcoming = document.querySelectorAll(".upcoming")
-        const forecast =  data.weather
-        const today = forecast[0]
-        const tomorrow = forecast[1]
-        const after = forecast[2]
-    
+      
         upcoming[0].innerHTML = `
-            <h4>Today:</h4>
-            <h4>Average Temperature:</h4>
+            <h5>Today:</h5>
+            <h5>Average Temperature:</h5>
             <p>${today.avgtempF}°F</p>
-            <h4>Max Temperature:</h4>
+            <h5>Max Temperature:</h5>
             <p>${today.maxtempF}°F</p>
-            <h4>Min Temperature:</h4>
+            <h5>Min Temperature:</h5>
             <p>${today.mintempF}°F</p>
         `
         upcoming[1].innerHTML = `
-            <h4>Tomorrow:</h4>
-            <h4>Average Temperature:</h4>
+            <h5>Tomorrow:</h5>
+            <h5>Average Temperature:</h5>
             <p>${tomorrow.avgtempF}°F</p>
-            <h4>Max Temperature:</h4>
+            <h5>Max Temperature:</h5>
             <p>${tomorrow.maxtempF}°F</p>
-            <h4>Min Temperature:</h4>
+            <h5>Min Temperature:</h5>
             <p>${tomorrow.mintempF}°F</p>
         `
         upcoming[2].innerHTML = `
-            <h4>Day After Tomorrow: </h4>
-            <h4>Average Temperature:</h4>
+            <h5>Day After Tomorrow: </h5>
+            <h5>Average Temperature:</h5>
             <p>${after.avgtempF}°F</p>
-            <h4>Max Temperature:</h4>
+            <h5>Max Temperature:</h5>
             <p>${after.maxtempF}°F</p>
-            <h4>Min Temperature:</h4>
+            <h5>Min Temperature:</h5>
             <p>${after.mintempF}°F</p>
         `
         upcoming.append(main)
-
+       
     })
     .catch((error) =>{
         console.log(error);
     });
     form.reset();
 
-});
-const converter = document.querySelector("#temperature-converter")
-const farenheit = document.querySelector("#to-f")
-const celsius = document.querySelector("#to-c")
-const convertedTemp = document.querySelector("#converted-temperature")
-let value = 0
-
-converter.addEventListener("submit", (event)=> {
+}
+form.addEventListener("submit", (event)=> {
     event.preventDefault()
-    // const convertInput = document.querySelector("temp-to-convert")
-   const convertInput = event.target[0].value
+    const locationInput = event.target[0].value
+    searchWeather(locationInput, true)
+});
+
+
+// CONVERTING TEMPERATURE WIDGET
+
+    const converter = document.querySelector("#temperature-converter")
+    const fahrenheit = document.querySelector("#to-f")
+    const celsius = document.querySelector("#to-c")
+    const convertedTemp = document.querySelector("#converted-temperature")
+    let value = 0
+
+    converter.addEventListener("submit", (event)=> {
+    event.preventDefault()
+    //  const convertInput = document.querySelector("temp-to-convert")
+        const convertInput = event.target[0].value
+        console.log(event)
     
-   if(celsius.checked){
-      value += (convertInput * 9/5) + 32
-      convertedTemp.innerText = value
+   if(celsius.checked ){
+      value = (convertInput * 9/5) + 32
+      convertedTemp.textContent = value.toFixed(2)
     console.log(value)
 }
-    else { 
-        value += (convertInput - 32) * 5/9
-        convertedTemp.innerText = value
+    else if (fahrenheit.checked) { 
+        value = (convertInput - 32) * 5/9
+        convertedTemp.textContent = value.toFixed(2)
         console.log(value)
-        
 }
+
  })
- 
+
+
+
+
